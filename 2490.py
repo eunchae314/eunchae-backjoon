f504c24096fa3549ed05ae5fbd1e5e5bf4f4169f
eunchae314 <aquariumc25@gmail.com>
2026-03-24 02:52:46 +0900

    Add files via upload

---
+++ 2490.py
@@ -0,0 +1,14 @@
+for _ in range(3):
+    arr = list(map(int, input().split()))
+    total = sum(arr)
+
+    if total == 0:
+        print("D")
+    elif total == 1:
+        print("C")
+    elif total == 2:
+        print("B")
+    elif total == 3:
+        print("A")
+    else:
+        print("E")
